3b3bac86879205ec68372513e956c4fb2a915b81
Mohammad SHakiB <mdshakib6477@gmail.com>
2023-10-22 13:52:59 +0600

    Task 2

---
+++ TASK2/TASK2/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TASK2.EF;
+
+namespace TASK2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new SHOPEntities();
+            var data = db.Products.Include("Category").ToList();
+            return View(data);
+        }
+
+        public ActionResult Buy()
+        {
+            var db = new SHOPEntities();
+            var data = db.Products.ToList();
+            return View(data);
+        }
+
+        
+
+    }
+}
